95527985cfd8f39ceaa5a1fc74c8417a4ee733c9
Himanshu Singh <hsksingh20072005@gmail.com>
2024-06-29 13:45:44 +0530

    adding configuration

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
   "stevearc/oil.nvim",
   config = function()
     local oil = require("oil")
-    oil.setup()
+    oil.setup({})
     vim.keymap.set("n", "-", oil.toggle_float, {})
   end,
 }
